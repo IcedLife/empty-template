6d997d187b1d5ef0ed160a1501b774baa58d659b
IcedLife <reachingfoul@gmail.com>
2024-12-08 20:58:13 -0600

    remove observable implementation

--- server.js
+++ server.js
@@ -25,38 +25,50 @@ app.use(express.static('app'));
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`);
+	console.log(`Server started at http://localhost:${PORT}`);
 });
 
 // Create a database connection
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'employees',
+	host: '127.0.0.1',
+	user: 'root',
+	password: 'root',
+	database: 'employees',
 });
 
 
 // API endpoint to fetch SQL data
 app.get('/api/departments', (req, res) => {
-    const query = `
-        SELECT d.dept_name AS Department, t.title AS Title, COUNT(*) AS Count
-        FROM employees e
-        JOIN dept_emp de ON e.emp_no = de.emp_no
-        JOIN departments d ON de.dept_no = d.dept_no
-        JOIN titles t ON e.emp_no = t.emp_no
-        WHERE d.dept_name = 'Customer Service' OR d.dept_name = 'Development'
-        GROUP BY d.dept_name, t.title
-        ORDER BY d.dept_name, t.title;
-    `;
-    connection.query(query, (err, results) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Server Error');
-        } else {
-            res.json(results); // Send results as JSON
-        }
-    });
+	const query = `
+		SELECT 
+			d.dept_name AS Department, 
+			t.title AS Title, 
+			e.gender AS Gender, 
+			COUNT(*) AS Count
+		FROM 
+			employees e
+		JOIN 
+			dept_emp de ON e.emp_no = de.emp_no
+		JOIN 
+			departments d ON de.dept_no = d.dept_no
+		JOIN 
+			titles t ON e.emp_no = t.emp_no
+		WHERE 
+			d.dept_name = 'Customer Service' OR d.dept_name = 'Development'
+		GROUP BY 
+			d.dept_name, t.title, e.gender
+		ORDER BY 
+			d.dept_name, t.title, e.gender;
+		`;
+
+	connection.query(query, (err, results) => {
+		if (err) {
+			console.error(err);
+			res.status(500).send('Server Error');
+		} else {
+			res.json(results); // Send results as JSON
+		}
+	});
 });
 
 
